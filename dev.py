77b7f1576f91589a07fce3168a1dd7b834567632
Harry Purdue <purdue.harry@gmail.com>
2026-03-16 19:39:41 -0500

    removing dev file

--- dev.py
+++
@@ -1,9 +0,0 @@
-from helper import Helper
-import customTasks
-
-h = Helper("config.yml")
-res = h.run(task = customTasks.command, command = "show version", use_textfsm = True)
-
-for host in res:
-    print(res[host][0].result)
-    breakpoint()
